c0bcbee152a385bad28eb3b5933e539cbdb17b15
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-08-15 11:39:25 +0530

    Create ping.py

---
+++ TagAll/modules/ping.py
@@ -0,0 +1,22 @@
+import time
+
+from Abg.helpers.human_read import get_readable_time
+from Abg.helpers.ratelimit import ratelimiter
+
+from pyrogram import Client
+from pyrogram.types import Message
+
+from TagAll import Abishnoi, StartTime
+
+
+@Abishnoi.on_cmd(["ping", "pong", "alive"])
+@ratelimiter
+async def ping(self: Client, ctx: Message):
+    currentTime = get_readable_time(time.time() - StartTime)
+    start_t = time.time()
+    rm = await ctx.reply_msg("💫 ᴘᴏɴɢ !!...")
+    end_t = time.time()
+    time_taken_s = round(end_t - start_t, 3)
+    await rm.edit_msg(
+        f"<b>ᴛᴀɢ ᴀʟʟ ᴘɪɴɢ:</b> <code>{time_taken_s}ᴍs</code>\n<b>ᴜᴘᴛɪᴍᴇ:</b> <code>{currentTime}</code>"
+    )
