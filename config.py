c4f2905f94592b0e03662755dd79bf25e1e483d7
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-08-15 11:30:56 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+TOKEN = getenv("TOKEN")
+MONGO_DB_URL = getenv("MONGO_DB_URL", None)
+
+OWNER_ID = int(getenv("OWNER_ID", 5938660179))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
+
+DEV_USERS = set(int(x) for x in getenv("DEV_USERS", "").split())
+LOGGER_ID = getenv("LOGGER_ID", "-1001819078701")
